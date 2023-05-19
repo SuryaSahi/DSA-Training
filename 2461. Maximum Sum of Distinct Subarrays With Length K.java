4e262333ad095b399784354d2f185ae4b4f46153
Surya pratap sahi <suryasahi900@gmail.com>
2023-05-19 14:06:38 +0530

    Sliding Window

--- 2461. Maximum Sum of Distinct Subarrays With Length K.java
+++ 2461. Maximum Sum of Distinct Subarrays With Length K.java
@@ -1,34 +1,28 @@
 class Solution {
     public long maximumSubarraySum(int[] nums, int k) {
-    int i = 0, j = 0, len = nums.length;
-        long maxSum = 0;
-        long local = 0;
-        HashSet<Integer> set = new HashSet<>();
-
-        while(j < len) {
-            int curr = nums[j];
-            local += curr;
-            if(set.contains(curr)) {  
-                while(i < j && set.contains(curr)) {
-                   int first = nums[i++];
-                   local -= first;
-                   set.remove(first);
-                }
-            } 
-            if (j - i + 1 < k) {  
-                set.add(curr);
-                j++;
-            } else if (j - i + 1 == k) { 
-                maxSum = Math.max(maxSum, local); 
-                int first = nums[i]; 
-                local -= first;
-                set.remove(first);
-                set.add(curr);
-                j++;
-                i++;
-            }
+    long maxsum = 0;
+    long currsum = 0;
+    if(k > nums.length)
+    {
+        return 0;
+    }
+    HashSet<Integer> set = new HashSet<>();
+    for(int i = 0, j = 0; i<nums.length; i++)
+    {
+        int curr = nums[i];
+        while(set.size() == k || set.contains(curr))
+        {
+            currsum -=nums[j];
+            set.remove(nums[j]);
+            j++;
         }
-
-        return maxSum;
+        set.add(curr);
+        currsum +=curr;
+        if( set.size() == k && maxsum < currsum)
+        {
+            maxsum = currsum;
+        }
+    }
+     return maxsum;
     }
 }
